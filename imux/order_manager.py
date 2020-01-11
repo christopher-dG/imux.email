397a9769017dc547587d2f5bd8e077205d6be2e3
Chris de Graaf <chrisadegraaf@gmail.com>
2020-01-11 18:55:59 +0700

    Do payments through the Checkout platform instead

--- imux/order_manager.py
+++ imux/order_manager.py
@@ -1,72 +1,81 @@
-from dataclasses import dataclass
 from datetime import timedelta
 from typing import Any, Dict, List, Optional
 
 import stripe
 
-from stripe import PaymentIntent, Webhook
+from stripe import Webhook
+from stripe.checkout import Session
 from stripe.error import SignatureVerificationError
 
-from . import ORDERS, STRIPE_KEY, STRIPE_WEBHOOK
+from . import DOMAIN, ORDERS, STRIPE_KEY, STRIPE_WEBHOOK
 from .account_manager import Account
 
 stripe.api_key = STRIPE_KEY
-CREATED = "created"
-CANCELLED = "cancelled"
-FAILED = "failed"
+PENDING = "pending"
 SUCCEEDED = "succeeded"
-FULLFILLED = "fulfilled"
 
 
-@dataclass
 class Order:
-    client_secret: int
-    price: int
-    state: str
-    recipients: List[str]
-    weeks: int
+    def __init__(
+        self, *, id: str, price: int, state: str, recipients: List[str], weeks: int
+    ) -> None:
+        self.id = id
+        self.price = price
+        self.state = state
+        self.recipients = recipients
+        self.weeks = weeks
 
     @staticmethod
-    def new(*, weeks: int, recipients: List[str]) -> "Order":
-        price = Order._price(recipients=len(recipients), weeks=weeks)
-        intent = PaymentIntent.create(amount=price, currency="usd")
+    def new(*, recipients: List[str], weeks: int) -> "Order":
+        price = Order._price(n_recipients=len(recipients), n_weeks=weeks)
+        session = Session.create(
+            cancel_url=f"https://{DOMAIN}?cancelled=true&session_id={{CHECKOUT_SESSION_ID}}",  # noqa: E501
+            success_url=f"https://{DOMAIN}/success.html&session_id={{CHECKOUT_SESSION_ID}}",  # noqa: E501
+            payment_method_types=["card"],
+            line_items=[
+                {
+                    "amount": price,
+                    "currency": "usd",
+                    "name": "Shared email address from imux.email",
+                    "quantity": 1,
+                },
+            ],
+        )
         order = Order(
-            client_secret=intent.client_secret,
+            id=session.id,
             price=price,
             recipients=recipients,
-            state=CREATED,
+            state=PENDING,
             weeks=weeks,
         )
         order._put()
         return order
 
     @staticmethod
-    def from_intent(intent: PaymentIntent) -> Optional["Order"]:
-        resp = ORDERS.get_item(Key={"client_secret": intent.client_secret})
+    def from_session(session: Session) -> Optional["Order"]:
+        resp = ORDERS.get_item(Key={"id": session.id})
         if "Item" not in resp:
             return None
         item = resp["Item"]
         return Order(
-            client_secret=item["client_secret"],
+            id=item["id"],
             price=item["price"],
             recipients=item["recipients"],
             state=item["state"],
             weeks=item["weeks"],
         )
 
-    def on_cancel(self) -> None:
-        self._update(state=CANCELLED)
-
-    def on_failure(self) -> None:
-        self._update(state=FAILED)
-
-    def on_success(self) -> None:
-        Account.new(self.recipients, timedelta(weeks=self.weeks))
+    def on_completed(self) -> None:
+        account = Account.new(self.recipients, timedelta(weeks=self.weeks))
+        self._update(state=SUCCEEDED)
+        recipients = ", ".join(recipient.address for recipient in account.recipients)
+        n = len(account.recipients)
+        print(f"Created new account {account.uuid} for {n} recipients: {recipients}")
 
     def _put(self) -> None:
         ORDERS.put_item(
             Item={
-                "client_secret": self.client_secret,
+                "id": self.id,
                 "price": self.price,
                 "state": self.state,
                 "recipients": self.recipients,
@@ -79,12 +88,10 @@ class Order:
         for k, v in kwargs.values():
             self.__setattr__(k, v)
             updates[k] = {"Value": v, "Action": "PUT"}
-        ORDERS.update_item(
-            Key={"client_secret": self.client_secret}, AttributeUpdates=updates,
-        )
+        ORDERS.update_item(Key={"id": self.id}, AttributeUpdates=updates)
 
     @staticmethod
-    def _price(*, recipients: int, weeks: int) -> int:
+    def _price(*, n_recipients: int, n_weeks: int) -> int:
         return 500  # TODO
 
 
@@ -93,19 +100,11 @@ def webhook(data: Dict[str, Any], signature: str) -> int:
         event = Webhook.construct_from(data, signature, STRIPE_WEBHOOK)
     except (ValueError, SignatureVerificationError):
         return 400
-    scope, action = event.type.split(".", 1)
-    if scope != "payment_intent":
+    if event.type != "checkout.session.completed":
         return 400
-    order = Order.from_intent(event.data.object)
+    order = Order.from_session(event.data.object)
     if not order:
         print("Order was not found")
         return 500
-    if scope == "cancelled":
-        order.on_cancel()
-    elif scope == "payment_failure":
-        order.on_failure()
-    elif scope == "succeeded":
-        order.on_success()
-    else:
-        return 400
+    order.on_completed()
     return 200
--- imux/web_api.py
+++ imux/web_api.py
@@ -1,12 +1,14 @@
 from typing import Any, Dict, Tuple
+from urllib.parse import quote
 
-from flask import Flask, Response as FlaskResponse, request
+from flask import Flask, Response as FlaskResponse, redirect, request
+from werkzeug import Response as WerkzeugResponse
 
 from . import DOMAIN, order_manager
 from .account_manager import Account
 from .order_manager import Order
 
-Response = Tuple[Dict[str, Any], int]
+JSONResponse = Tuple[Dict[str, Any], int]
 app = Flask(__name__)
 
 
@@ -17,23 +19,23 @@ def cors(response: FlaskResponse) -> FlaskResponse:
 
 
 @app.route("/payments", methods=["POST"])
-def create_order() -> Response:
-    recipients = [r.strip() for r in request.form.get("recipients", "").split(",")]
+def create_order() -> WerkzeugResponse:
+    recipients = [r for r in request.form.getlist("recipients") if r]
     weeks = request.form.get("weeks", "")
     try:
         weeks = int(weeks)
     except ValueError:
-        return {"error": "Invalid input for weeks, expected an integer"}, 400
-    if not recipients:
-        return {"error": "No recipients were specified"}, 400
+        return _redirect("/", message="Invalid number input for weeks")
     if weeks <= 0:
-        return {"error": "Number of weeks should be positive"}, 400
+        return _redirect("/", message="Invalid number input for weeks")
+    if not recipients:
+        return _redirect("/", message="You need to enter at least one recipient")
     order = Order.new(recipients=recipients, weeks=weeks)
-    return {"client_secret": order.client_secret}, 200
+    return _redirect("/redirect", id=order.id)
 
 
 @app.route("/payments/webhook", methods=["POST"])
-def payment_webhook() -> Response:
+def payment_webhook() -> JSONResponse:
     status = order_manager.webhook(
         request.json, request.headers.get("HTTP_STRIPE_SIGNATURE", ""),
     )
@@ -41,7 +43,7 @@ def payment_webhook() -> Response:
 
 
 @app.route("/unsubscribe/<uuid>/<token>", methods=["POST"])
-def unsubscribe(uuid: str, token: str) -> Response:
+def unsubscribe(uuid: str, token: str) -> JSONResponse:
     account = Account.get(uuid)
     if not account:
         return {"error": "The email account was not found"}, 400
@@ -50,3 +52,8 @@ def unsubscribe(uuid: str, token: str) -> Response:
         return {"error": "The email recipient was not found"}, 400
     account.unsubscribe(recipient)
     return {}, 200
+
+
+def _redirect(destination: str, **kwargs: str) -> WerkzeugResponse:
+    query = "&".join(f"{k}={quote(v)}" for k, v in kwargs.items())
+    return redirect(f"{destination}?{query}")
